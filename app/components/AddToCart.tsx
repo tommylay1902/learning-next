f3e5ddb1f5952632a6d51d22d9badd58f9de6f7c
tommylay1902 <tommylay.c@gmail.com>
2023-10-15 08:19:46 -0700

    static rendering vs dynamic rendering

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div> <button onClick={() => console.log("click")}>Add To Cart</button></div>
+  )
+}
+
+export default AddToCart
